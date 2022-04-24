1fa1edf4ee7f6f19c0d8186e22ff5031b8369865
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-04-24 13:42:35 -0300

    refactor: 💡 remove default nest controller and service

--- packages/challenges/src/app.module.ts
+++ packages/challenges/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
 import { MessagingModule } from './messaging/messaging.module';
 
 @Module({
   imports: [DatabaseModule, HttpModule, MessagingModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
